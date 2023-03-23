6cd0641afe35deb9b9f75422b0636f0411f77766
ti-metiss <coutenay.yoan@hotmail.fr>
2023-03-23 13:15:12 +0100

    connexion DB

--- back/app.js
+++ back/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const userRoute = require("./route/userRoute");
 
 const app = express();
+
+app.use("/user", userRoute);
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "page par defaut",
+  });
+});
 
 module.exports = app;
--- back/server.js
+++ back/server.js
@@ -1,5 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+const DB = process.env.DB_SERVER.replace("<password>", process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`connexion database`);
+  })
+  .catch((err) => {
+    console.log(`erreur database error: ${err}`);
+  });
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (_) => {
